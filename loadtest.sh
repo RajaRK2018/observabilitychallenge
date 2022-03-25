973e97525fa6fcab549c5bf08d13e3e3f42df92e
RajaRK2018 <46085909+RajaRK2018@users.noreply.github.com>
2022-03-25 08:37:29 +0530

    Update loadtest.sh

--- loadtest.sh
+++ loadtest.sh
@@ -1,13 +1,13 @@
 for (( ; ; ))
 do
-   sudo ab -n 100 -c 100 http://{{EC2PublicIP}}:5000/
+   sudo ab -n 100 -c 10 http://{{EC2PublicIP}}:5000/
    sleep 10s
-   sudo ab -n 10 -c 100 http://{{EC2PublicIP}}:5000/redirect
+   sudo ab -n 10 -c 1 http://{{EC2PublicIP}}:5000/redirect
    sleep 10s
-   sudo ab -n 40 -c 100 http://{{EC2PublicIP}}:5000/bad_request
+   sudo ab -n 40 -c 4 http://{{EC2PublicIP}}:5000/bad_request
    sleep 10s
-   sudo ab -n 30 -c 100 http://{{EC2PublicIP}}:5000/exception
+   sudo ab -n 30 -c 3 http://{{EC2PublicIP}}:5000/exception
    sleep 10s
-   sudo ab -n 20 -c 100 http://{{EC2PublicIP}}:5000/lorum
+   sudo ab -n 20 -c 2 http://{{EC2PublicIP}}:5000/lorum
    sleep 10s
 done

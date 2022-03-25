050bef165a23cc7202c1627b8e55b1c37bcd8469
RajaRK2018 <46085909+RajaRK2018@users.noreply.github.com>
2022-03-25 07:51:41 +0530

    Create prom-setup.sh

---
+++ prom-setup.sh
@@ -0,0 +1,8 @@
+wget https://github.com/prometheus/prometheus/releases/download/v2.33.3/prometheus-2.33.3.linux-amd64.tar.gz
+sleep 5s
+tar -xvf prometheus-2.33.3.linux-amd64.tar.gz
+sleep 2s
+cd prometheus-2.33.3.linux-amd64
+sleep 2s
+sudo vi prometheus.yml
+

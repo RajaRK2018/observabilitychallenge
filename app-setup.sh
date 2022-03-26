5fc61e5d248241e18ca14d49771cf754e0608199
RajaRK2018 <46085909+RajaRK2018@users.noreply.github.com>
2022-03-26 16:45:09 +0530

    Update app-setup.sh

--- app-setup.sh
+++ app-setup.sh
@@ -1,28 +1,26 @@
 #!/bin/sh
 
-cd /usr/bin
-sleep 1s
-sudo mkdir obs
+mkdir obs
 sleep 1s
 cd obs
 sleep 1s
-sudo pip3 install flask
+pip3 install flask
 sleep 5s
-sudo pip3 install prometheus-flask-exporter
+pip3 install prometheus-flask-exporter
 sleep 5s
-sudo wget https://raw.githubusercontent.com/RajaRK2018/observabilitychallenge/main/flask-app.py
+wget https://raw.githubusercontent.com/RajaRK2018/observabilitychallenge/main/flask-app.py
 sleep 3s
-sudo chmod 777 flask-app.py
+chmod 777 flask-app.py
 sleep 1s
-sudo wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
+wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
 sleep 3s
-sudo tar -xvf node_exporter-1.3.1.linux-amd64.tar.gz
+tar -xvf node_exporter-1.3.1.linux-amd64.tar.gz
 sleep 1s
-sudo wget https://raw.githubusercontent.com/RajaRK2018/observabilitychallenge/main/start-flask-app.sh
+wget https://raw.githubusercontent.com/RajaRK2018/observabilitychallenge/main/start-flask-app.sh
 sleep 3s
-sudo chmod +x start-flask-app.sh
+chmod +x start-flask-app.sh
 sleep 1s
-sudo wget https://raw.githubusercontent.com/RajaRK2018/observabilitychallenge/main/start-flask-app.service
+wget https://raw.githubusercontent.com/RajaRK2018/observabilitychallenge/main/start-flask-app.service
 sleep 3s
 sudo cp start-flask-app.service /lib/systemd/system/
 sleep 2s
@@ -33,4 +31,3 @@ sleep 2s
 sudo systemctl start start-flask-app.service
 sleep 2s
 sudo systemctl status start-flask-app.service -l
-

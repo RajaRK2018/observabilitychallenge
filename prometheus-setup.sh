54e2f941823a5c420297865cc21ba934f861d9ef
RajaRK2018 <46085909+RajaRK2018@users.noreply.github.com>
2022-03-26 15:17:12 +0530

    Update prometheus-setup.sh

--- prometheus-setup.sh
+++ prometheus-setup.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 sudo wget https://github.com/prometheus/prometheus/releases/download/v2.33.3/prometheus-2.33.3.linux-amd64.tar.gz
 sleep 3s
 sudo tar -xvf prometheus-2.33.3.linux-amd64.tar.gz
@@ -5,3 +7,4 @@ sleep 1s
 cd prometheus-2.33.3.linux-amd64
 sleep 1s
 sudo vi prometheus.yml
+

0303fdf4721471137933b1437148708b8f3f13db
RajaRK2018 <46085909+RajaRK2018@users.noreply.github.com>
2022-03-26 15:10:04 +0530

    Create prometheus-setup.sh

---
+++ prometheus-setup.sh
@@ -0,0 +1,7 @@
+sudo wget https://github.com/prometheus/prometheus/releases/download/v2.33.3/prometheus-2.33.3.linux-amd64.tar.gz
+sleep 3s
+sudo tar -xvf prometheus-2.33.3.linux-amd64.tar.gz
+sleep 1s
+cd prometheus-2.33.3.linux-amd64
+sleep 1s
+sudo vi prometheus.yml

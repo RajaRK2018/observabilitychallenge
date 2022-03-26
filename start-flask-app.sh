80ba8113a3a6ceba322b109bc5988613176313ea
RajaRK2018 <46085909+RajaRK2018@users.noreply.github.com>
2022-03-26 13:49:49 +0530

    Update start-flask-app.sh

--- start-flask-app.sh
+++ start-flask-app.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/bin/obs/python3 flask-app.py & /usr/bin/obs/node_exporter-1.3.1.linux-amd64/node_exporter &
+python3 /usr/bin/obs/flask-app.py & /usr/bin/obs/node_exporter-1.3.1.linux-amd64/node_exporter &

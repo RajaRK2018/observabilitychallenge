056e1ccb9462833eb6795c2c77ae756133964e92
RajaRK2018 <46085909+RajaRK2018@users.noreply.github.com>
2022-03-26 16:16:34 +0530

    Update prom-setup.sh

--- prom-setup.sh
+++ prom-setup.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-sudo mkdir prom
+mkdir prom
 sleep 1s
 cd prom
 sleep 1s
-sudo wget https://github.com/prometheus/prometheus/releases/download/v2.33.3/prometheus-2.33.3.linux-amd64.tar.gz
+wget https://github.com/prometheus/prometheus/releases/download/v2.33.3/prometheus-2.33.3.linux-amd64.tar.gz
 sleep 3s
-sudo tar -xvf prometheus-2.33.3.linux-amd64.tar.gz
+tar -xvf prometheus-2.33.3.linux-amd64.tar.gz
 sleep 1s
-sudo wget https://raw.githubusercontent.com/RajaRK2018/observabilitychallenge/main/start-prom-app.sh
+wget https://raw.githubusercontent.com/RajaRK2018/observabilitychallenge/main/start-prom-app.sh
 sleep 3s
-sudo chmod +x start-prom-app.sh
+chmod +x start-prom-app.sh
 sleep 1s
-sudo wget https://raw.githubusercontent.com/RajaRK2018/observabilitychallenge/main/start-prom-app.service
+wget https://raw.githubusercontent.com/RajaRK2018/observabilitychallenge/main/start-prom-app.service
 sleep 3s
-sudo cp start-prom-app.service /lib/systemd/system/
+cp start-prom-app.service /lib/systemd/system/
 sleep 2s
-sudo systemctl daemon-reload
+systemctl daemon-reload
 sleep 2s
-sudo systemctl enable start-prom-app.service
+systemctl enable start-prom-app.service
 sleep 2s
-sudo systemctl start start-prom-app.service
+systemctl start start-prom-app.service
 sleep 2s
-sudo systemctl status start-prom-app.service -l
+systemctl status start-prom-app.service -l

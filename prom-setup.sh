2a03a29b6c1f1978a3ef24b7af7323e73734118c
RajaRK2018 <46085909+RajaRK2018@users.noreply.github.com>
2022-03-26 16:23:11 +0530

    Update prom-setup.sh

--- prom-setup.sh
+++ prom-setup.sh
@@ -10,16 +10,16 @@ tar -xvf prometheus-2.33.3.linux-amd64.tar.gz
 sleep 1s
 wget https://raw.githubusercontent.com/RajaRK2018/observabilitychallenge/main/start-prom-app.sh
 sleep 3s
-sudo chmod +x start-prom-app.sh
+chmod +x start-prom-app.sh
 sleep 1s
 wget https://raw.githubusercontent.com/RajaRK2018/observabilitychallenge/main/start-prom-app.service
 sleep 3s
-sudo cp start-prom-app.service /lib/systemd/system/
+cp start-prom-app.service /lib/systemd/system/
 sleep 2s
-sudo systemctl daemon-reload
+systemctl daemon-reload
 sleep 2s
-sudo systemctl enable start-prom-app.service
+systemctl enable start-prom-app.service
 sleep 2s
-sudo systemctl start start-prom-app.service
+systemctl start start-prom-app.service
 sleep 2s
 systemctl status start-prom-app.service -l

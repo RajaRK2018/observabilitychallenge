12ccd79c7a1daf4b18a0313b588ed412f514c78c
RajaRK2018 <46085909+RajaRK2018@users.noreply.github.com>
2022-03-27 17:26:35 +0530

    Update flask-app.py

--- flask-app.py
+++ flask-app.py
@@ -1,20 +1,26 @@
+from random import randint
+from time import sleep
 from flask import Flask,redirect
 from prometheus_flask_exporter import PrometheusMetrics
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 @app.route('/')
 def main():
+ sleep(randint(1,2))
  return 'Welcome to Flask App'
 @app.route('/redirect')
 def handle_redirect():
  return redirect("/", code=302)
 @app.route('/bad_request')
-def bad_request():
+def bad_request(): 
+ sleep(randint(3,4))
  return 'bad request!', 400
 @app.errorhandler(404)
 def pageNotFound(error):
+ sleep(randint(4,5))
  return 'page not found!', 404
 @app.route('/exception')
-def exception():
+def exception(): 
+ sleep(randint(6,8))
  return exception("Causing Exception")
 app.run(host='0.0.0.0')
